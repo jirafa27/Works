97b2b47b8eddda5f21c395b8cf25d8a29f2d5751
jirafa27 <33696138+jirafa27@users.noreply.github.com>
2019-04-27 22:36:57 +0300

    Update ObjectBox.java

--- src/lesson04/task02/ObjectBox.java
+++ src/lesson04/task02/ObjectBox.java
@@ -3,17 +3,34 @@ package lesson04.task02;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+/**
+ * Метод для работы с объектами
+ */
 public class ObjectBox {
     public Set<Object> set = new LinkedHashSet<>();
 
+    /**
+     *
+     * @param o - объект для добавления
+     */
     public void addObject(Object o)
     {
         set.add(o);
     }
+
+    /**
+     *
+     * @param o - объект для удаления
+     */
     public void deleteObject(Object o)
     {
         set.remove(o);
     }
+
+    /**
+     *
+     * @return строку из объектов
+     */
     public String dump()
     {
         StringBuilder stringBuilder = new StringBuilder();
@@ -25,3 +42,4 @@ public class ObjectBox {
         return stringBuilder.toString();
     }
 }
+
